8ee764419b8e432d1831c7dde95544962552fdd8
oumarsig <oumarniang@MacBook-Pro-de-Oumar.local>
2021-02-14 23:31:04 +0100

    Liste

---
+++ listMois.py
@@ -0,0 +1,7 @@
+t1=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+t2=["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Aout", "Septembre", "Octobre", "Novembre", "Décembre"]
+t3=[]
+for i in range(len(t1)):
+    t3.append(t2[i])
+    t3.append(t1[i])
+print(t3)
---
+++ suit12number.py
@@ -0,0 +1,5 @@
+i=1
+while i <= 12:
+    res=3**i
+    print (res)
+    i+=1
